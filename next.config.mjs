e37244e8a0a376d53ab551c5736909855775a861
ramo-dev <annuar.dev@gmail.com>
2024-07-14 13:27:21 +0300

    next config to handle 3rd party images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['images.unsplash.com', 'fakestoreapi.com', 'img.freepik.com'],
+      },
+};
 
 export default nextConfig;
